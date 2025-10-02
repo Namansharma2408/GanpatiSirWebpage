37c4d906fb6da2cc1b18c0e0df4ecb49fb66ed7e
Namansharma2408 <sharmanaman4002@gmail.com>
2025-10-02 06:54:00 +0530

    all done

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import dynamic from "next/dynamic";
 const Navbar = dynamic(() => import("./common/navbar"));
 
 const Footer = dynamic(() => import("./common/footer"));
-
-export { Navbar, Footer };
+const LoadingBackground = dynamic(() => import("./common/LoadingBackground"))
+const TestTubeLoader = dynamic(() => import("./common/TestTubeAnimation"))
+export { Navbar, Footer,LoadingBackground,TestTubeLoader };
